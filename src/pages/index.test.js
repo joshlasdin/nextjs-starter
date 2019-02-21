0aa49d44eb80bf985f83728c2dd54b7dc9e1d562
Josh Lasdin <joshlasdindesign@gmail.com>
2019-02-21 01:43:51 -0500

    Upgrade packages, add simple test

---
+++ src/pages/index.test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render } from '@test/utils';
+import IndexPage from './index';
+
+it('renders the index page', () => {
+    render(<IndexPage />);
+});
--- test/wrapper.js
+++ test/wrapper.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { MockedProvider } from 'react-apollo/test-utils';
 
-const TestWrapper = ({ children }) => <div>{children}</div>;
+const TestWrapper = ({ mocks, children }) => (
+    <MockedProvider mocks={mocks} addTypeName={false}>
+        {children}
+    </MockedProvider>
+);
 
 TestWrapper.propTypes = {
+    mocks: PropTypes.arrayOf(
+        PropTypes.shape({
+            request: PropTypes.shape({
+                query: PropTypes.string,
+            }),
+            result: PropTypes.shape({
+                data: PropTypes.shape({}),
+            }),
+        })
+    ),
     children: PropTypes.node,
 };
 
 TestWrapper.defaultProps = {
+    mocks: [],
     children: null,
 };
 
