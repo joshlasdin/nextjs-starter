cc8506096524fadff63855aaeefc766cb112b341
Josh Lasdin <joshlasdindesign@gmail.com>
2019-07-11 12:34:53 -0400

    Upgrade jest-dom

--- test/index.test.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { render } from '@test/utils';
-import IndexPage from '../src/pages/index';
-
-it('renders the index page', () => {
-    render(<IndexPage />);
-});
